21ca0c8918d8dd5be109d999332ff14f54b13b74
drawkcab <MJRoche@mail.usf.edu>
2018-01-25 21:27:15 -0500

    logic for add store and store form

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -5,3 +5,7 @@ exports.homePage = (req, res) => {
 exports.addStore = (req, res) => {
 	res.render('addStore', { title: 'Add Store' });
 }
+
+exports.createStore = (req, res) => {
+	res.json(req.body);
+}
